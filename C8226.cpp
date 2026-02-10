ebd498e2d6f2ea17f6472155e0928405fd23c17c
Swayam Atal <atalswayam28@gmail.com>
2026-02-10 09:54:02 +0530

    div2-c

--- C8226.cpp
+++ C8226.cpp
@@ -1,79 +1,95 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define pll pair<ll, ll>
+#define vll vector<ll>
+const int N = 1e5 + 11, mod = 1e9 + 7;
+ll max(ll a, ll b) { return ((a > b) ? a : b); }
+ll min(ll a, ll b) { return ((a > b) ? b : a); }
 
-void solve() {
-    int n, k;
-    cin >> n >> k;
-    vector<int> col_masks(n, 0);
-
-    for (int i = 0; i < k; ++i) {
-        string s;
-        cin >> s;
-        for (int j = 0; j < n; ++j) {
-            col_masks[j] |= (1 << (s[j] - 'a'));
-        }
-    }
-
-    vector<int> divisors;
-    for (int d = 1; d * d <= n; ++d) {
-        if (n % d == 0) {
-            divisors.push_back(d);
-            if (d * d != n) {
-                divisors.push_back(n / d);
-            }
-        }
-    }
-    sort(divisors.begin(), divisors.end());
-
-    for (int d : divisors) {
-        string pattern = "";
-        bool possible = true;
-
-        for (int i = 0; i < d; ++i) {
-            int current_mask = (1 << 26) - 1;
-            
-            for (int j = i; j < n; j += d) {
-                current_mask &= col_masks[j];
-                
-                // Optimization: If mask becomes 0, no character works for this position
-                if (current_mask == 0) break;
-            }
-
-            if (current_mask > 0) {
-                // Find the first set bit (smallest character)
-                // __builtin_ctz returns the number of trailing zeros
-                int char_idx = __builtin_ctz(current_mask); 
-                pattern += (char)('a' + char_idx);
-            } else {
-                possible = false;
-                break;
-            }
-        }
-
-        if (possible) {
-            string result = "";
-            for (int i = 0; i < n / d; ++i) {
-                result += pattern;
-            }
-            cout << result << endl;
-            return; // Found the minimal informativity, we are done
-        }
-    }
-}
-
-int main() {
-    // Optimize I/O operations for speed
+int main()
+{
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL), cout.tie(NULL);
 
     int t;
-    if (cin >> t) {
-        while (t--) {
-            solve();
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> divs;
+        for (int i = 1; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                divs.push_back(i);
+                if (i * i != n)
+                {
+                    divs.push_back(n / i);
+                }
+            }
+        }
+        vector<vector<bool>> has(n, vector<bool>(26, false));
+
+        for (int i = 0; i < k; i++)
+        {
+            string s;
+            cin >> s;
+            for (int j = 0; j < n; j++)
+            {
+                has[j][s[j] - 'a'] = true;
+            }
+        }
+        // cout << "divs: " << endl;
+        // for(auto d:divs) cout << d << " ";
+        sort(divs.begin(), divs.end());
+        string ans = "";
+        int d = divs.size();
+        for (int i = 0; i < d; i++)
+        {
+            int divisor = divs[i]; // 1
+            string pattern = "";
+            bool possible=true;
+            for (int h = 0; h < divisor; h++)
+            {
+                char fc = 0;
+                for (int c = 0; c < 26; c++)
+                {
+                    bool chok = true;
+                    for (int j = h; j < n; j += divisor)
+                    {
+                        if (!has[j][c])
+                        {
+                            chok = false;
+                            break;
+                        }
+                    }
+                    if (chok)
+                    {
+                        fc = (char)('a' + c);
+                        break;
+                    }
+                }
+                if (fc != 0)
+                    pattern += fc;
+                else
+                {
+                    possible = false;
+                    break;
+                }
+            }
+            if (possible)
+            {
+                string ans = "";
+                for (int z = 0; z < n / divisor; z++)
+                    ans += pattern;
+                cout << ans << endl;
+                break;
+            }
         }
     }
     return 0;
