83cecfe72e1568401f318e5a8dbfd90cc1cd541f
Swayam Atal <atalswayam28@gmail.com>
2026-03-29 20:11:46 +0530

    cf

--- fence.cpp
+++ fence.cpp
@@ -76,12 +76,28 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
-    while(t--)
-    {
-        
+    int n;cin >> n;
+    int k;cin >> k;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin >> a[i];
+    int l=0;
+    int r=k-1;
+    int sum=0;
+    for(int i=l;i<=r;i++) sum+=a[i];
+    l++;r++;
+    int h=sum;
+    int ans=1;
+    while(r<n){
+        h=(h-a[l-1]+a[r]);
+        if(h<sum)
+        {
+            sum=h;
+            ans=l+1;
+        }
+        r++;
+        l++;
     }
+    cout << ans << endl;
 
     return 0;
 }
